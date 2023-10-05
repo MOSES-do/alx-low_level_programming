910d7f325ba6e0efeae2d09fd6d86297488f71ed
Esumei Moses <archer.me24@gmail.com>
2023-10-05 11:57:30 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", 1);
+	printf("Size of a int: %d byte(s)", 4);
+	printf("Size of a long int: %d byte(s)", 8);
+	printf("Size of a long long int: %d byte(s)", 8);
+	printf("Size of a float: %d byte(s)", 4);
+	return (0);
+}
