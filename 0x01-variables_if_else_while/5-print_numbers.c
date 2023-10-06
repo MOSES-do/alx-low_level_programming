5c25be2888d2021f8ca62a3c05ffecc41897b61b
Esumei Moses <archer.me24@gmail.com>
2023-10-06 06:37:27 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+
+	int base = 0;
+
+	for (; base < 10; base++)
+	{
+		printf("%d", base);
+	}
+putchar('\n');
+return (0);
+}
+
+
+
