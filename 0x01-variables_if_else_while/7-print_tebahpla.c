c0dfefd9c9b250a6ba4c1a802dfc2506ec9df055
Esumei Moses <archer.me24@gmail.com>
2023-10-06 07:05:24 -0700

    reverseprint

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+
+	char alphabets = 122;
+
+	for (; alphabets >= 97; alphabets--)
+	{
+		putchar(alphabets);
+	}
+
+putchar('\n');
+
+return (0);
+
+}
