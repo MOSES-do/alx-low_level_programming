338832a44c31d3a6d9c370736effae3822ab5f65
Esumei Moses <archer.me24@gmail.com>
2023-10-06 07:22:37 -0700

    base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+
+	char a, b;
+
+	for (a = 0; a <= 9; a++)
+		putchar('0' + a);
+
+	for (b = 97; b <= 102; b++)
+		putchar(b);
+
+putchar('\n');
+
+return (0);
+}
