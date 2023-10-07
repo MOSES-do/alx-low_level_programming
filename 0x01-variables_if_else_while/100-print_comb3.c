3e5a6e4f7d775318d7dd88f14ede50f42961e815
Esumei Moses <archer.me24@gmail.com>
2023-10-06 22:49:04 -0700

    brain and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+
+
+int main(void)
+{
+	int num, num1;
+
+	for (num = 0; num <= 8; num++)
+	{
+		for (num1 = 1; num1 <= 9; num1++)
+		{
+			if (num1 != num)
+			{
+			putchar(48 + num);
+			putchar(48 + num1);
+
+			if (num != 0 || num1 != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+
+		}
+	}
+
+	putchar('\n');
+return (0);
+}
