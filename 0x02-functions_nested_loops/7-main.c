d6f571c23ff7df93fcaf38684282792ee9420da1
Esumei Moses <archer.me24@gmail.com>
2023-10-11 04:43:24 -0700

    main

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * main - invokes function
+ *
+ * Return: Always 0.
+ */
+
+
+ int main(void)
+ {
+
+	print_last_digit(98);
+	print_last_digit(0);
+	print_last_digit(-1024);
+	
+	
+	
+
+	_putchar('\n');
+
+	return (0);
+ }
