6504ea02cc9d672fd2a447774419f4fdde78c519
Esumei Moses <archer.me24@gmail.com>
2023-10-11 03:07:54 -0700

    ham

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _abs - Entry point
+*
+* @n: parameter of functions
+*
+* Return: Always 0;
+*/
+
+int _abs(int n)
+{
+	
+
+	_abs(n);
+
+	return (0);
+}
