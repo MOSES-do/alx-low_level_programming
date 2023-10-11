ac23e29b8b6c389d262d8985a173fd0ce312ffc6
Esumei Moses <archer.me24@gmail.com>
2023-10-11 05:41:08 -0700

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_time - Prints every minute of the day of from 00:00 to 23:59.
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int print_time(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - invoke time function
+*
+*/
+
+
+int main(void)
+{
+	print_time();
+
+	return (0);
+}
