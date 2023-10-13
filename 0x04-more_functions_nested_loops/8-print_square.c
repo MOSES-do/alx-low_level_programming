55778b8fb2d75c1c8bd0928cdddd4a32225cf22e
Esumei Moses <archer.me24@gmail.com>
2023-10-13 11:33:01 -0700

    8git

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+* print_square - print a square
+* @size: no of squares (column wise)
+*/
+
+
+void print_square(int size)
+{
+	int col, row;
+
+	if (size > 0)
+	{
+		for (col = 0; col < size; col++)
+		{
+			for (row = 0; row < size; row++)
+			{
+				_putchar('#');
+			}
+
+			if (col == size -1)
+				continue;
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
