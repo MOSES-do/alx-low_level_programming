bb4f482e7050f78f653e46659e7999e65d3d261c
Esumei Moses <archer.me24@gmail.com>
2023-10-13 03:20:16 -0700

    update on upper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,21 +1,41 @@
 #include "main.h"
-#include <stdio.h>
-#include <ctype.h>
+
 /**
 * main - Invokes function
 *
 * Return: Always 0 (Success)
 */
 
+
+void checkCasing(int n)
+{
+	int r;
+
+	r = _isupper(n);
+	
+
+	_putchar(n);
+	_putchar(':');
+	_putchar(' ');
+	_putchar('0' + r);
+	_putchar('\n');
+
+
+}
+
+
 int main(void)
 {
-
-	char c;
+	/**
+	char c, r;
 
     	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	r = _isupper(c);
+	printf("%c: %d\n", c, r);
+	*/
+
+	checkCasing('A');
+	checkCasing('a');
 
 	return (0);
 }
