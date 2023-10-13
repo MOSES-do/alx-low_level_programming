bf67ed3611d09ccea3097176821069d7a05ba1e6
Esumei Moses <archer.me24@gmail.com>
2023-10-13 09:01:04 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+* print_letter - print new lines based on parameter 'n'
+* @n: parameter
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
