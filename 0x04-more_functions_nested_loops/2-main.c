8bac5e36d47731241f6ad322fccf5b41bb6d7643
Esumei Moses <archer.me24@gmail.com>
2023-10-13 06:37:12 -0700

    multiply

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -1,19 +1,36 @@
 #include "main.h"
 
 
-
+/**
 void multiply(int a, int b)
 {
-	int r = (a * b);
+	int r;
+
+	r = mul(a, b);
+
+	_putchar(a);
+	_putchar(b);
 	
-	printf("%d\n", r);
+	_putchar(r);
+	_putchar('\n');
 }
+*/
 
 
 int main(void)
 {
-	multiply(98, 1024);
-	multiply(-402, 4096);
+
+	int a, b;
+
+	a = mul(98, 1024);
+	_putchar(48 + a);
+	_putchar('\n');
+
+
+	b = mul(-402, 4096);	
+	_putchar(48 + b);
+	_putchar('\n');
+
 
 	return (0);
 }
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -10,8 +10,7 @@
 
 int mul(int a, int b)
 {
-	return (a);
-	return (b);
+	return ((a) * (b));
 
 	return (0);
 }
