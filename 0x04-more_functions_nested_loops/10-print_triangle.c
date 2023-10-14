56d57142f5114e7ae03c511ed88e588422e14458
Esumei Moses <archer.me24@gmail.com>
2023-10-14 09:55:33 -0700

    print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* print_triangle - function
+* @size: parameter
+*
+*/
+
+
+void print_triangle(int size)
+{
+	int row, col, spaces;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (spaces = size; spaces > row; spaces--)
+			{
+				_putchar(' ');
+
+			}
+
+			for (col = 0; col <= row; col++)
+			{
+				_putchar('#');
+			}
+
+			if (col > size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+
+}
