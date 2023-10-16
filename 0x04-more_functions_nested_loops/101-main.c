c85dc0f799ca9860b3b6df4216255ad25c1046d1
Esumei Moses <archer.me24@gmail.com>
2023-10-16 10:23:16 -0700

    print

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* main - invoke print_number function
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+
+	print_number(98);
+	putchar('\n');
+
+
+	print_number(402);
+	putchar('\n');
+
+	print_number(1024);
+	putchar('\n');
+
+	print_number(0);
+	putchar('\n');
+
+	print_number(-98);
+	putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <math.h>
+
+
+/**
+* print_number - print an integer 
+* @n: parameter
+*/
+
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}
