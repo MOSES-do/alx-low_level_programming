929102238af80f687564f12f2b1d2d7beb2eae77
Esumei Moses <archer.me24@gmail.com>
2023-10-17 08:19:45 -0700

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* puts_half - print half of  a string
+*
+* @str: parameter
+*/
+
+void puts_half(char *str)
+{
+	int i = 0, length = 0, halve;
+
+	while (str[i++])
+		length++;
+
+	if (length % 2 != 0)
+		halve = (length - 1) / 2;
+	else
+		halve = length / 2;
+
+	for (i = halve; i <= length; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+
+}
