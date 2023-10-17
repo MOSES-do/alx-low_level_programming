b591f313e5d127120902c4aea2a4bd7489fed9fd
Esumei Moses <archer.me24@gmail.com>
2023-10-17 10:17:37 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+* _strcpy - function
+* @dest: param 1
+* @src: param 2
+*
+* Return: Always 0 (Success)
+*/
+
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
