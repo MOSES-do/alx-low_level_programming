afb86e95be1b21213210c6248e648a29866919e9
Esumei Moses <archer.me24@gmail.com>
2023-10-16 21:28:28 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+* main - invoke function
+*
+* Return: ALways 0 (Success
+*/
+
+
+int main(void)
+{
+	char *str;
+	int len;
+
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
+	
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <string.h>
+
+
+/**
+* _strlen - Outputs the number of string in param *s
+*
+* Return: Always 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+
+	char *str = s;
+
+	return strlen(str);
+
+	return (0);
+}
