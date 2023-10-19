689aad36e26e154646c939a1130d7e480b6c2d3e
Esumei Moses <archer.me24@gmail.com>
2023-10-18 18:04:26 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,27 @@
 #include <string.h>
-#include <stdio.h>
-
 
 /**
-* *_strncat - function to print source and dest in dest. 
-* Output of source is dependent on n
+* _strncat - function
 * @dest: param 1
 * @src: param 2
 * @n: param 3
+* Return: Always 0 Success
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	/*char *str;*/
-	
-	for (i = 0; i < n;  i++)
-	{
-		putchar(src[i]);
-		/*putchar(&src[i]);*/
-		/**str = &src[i];*/
-		/*strcat(dest, *src[i]);*/
-	}
-
-
+	/**
+	*int index = 0, dest_len = 0;
+	*
+	*while (dest[index++])
+	*	dest_len++;
+	*
+	*for (index = 0; src[index] && index < n; index++)
+	*	dest[dest_len++] = src[index];
+	*
+	*return (dest);
+	*/
+	strncat(dest, src, n);
 	return (dest);
 
 
