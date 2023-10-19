ddb840f47ccc9fcd2970be5d6905a4da7da78268
Esumei Moses <archer.me24@gmail.com>
2023-10-19 08:07:44 -0700

    reverse fn()

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;  
+
+	for (i = 0; i < n/2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i -1] = temp;
+	}
+
+	/*for (i = 0; i < n; i++)
+		printf("%d,", a[i]);
+	*/
+
+	/*printf("\n");*/
+}
