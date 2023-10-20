076c1199234ac382c5e00a1450e47ed58f9b2133
Esumei Moses <archer.me24@gmail.com>
2023-10-19 21:16:24 -0700

    4-rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,15 +12,24 @@
 
 void reverse_array(int *a, int n)
 {
+
 	int i, temp;
 
 	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
+		/*printf("%d temp:\n", temp);*/
 		a[i] = a[n - i - 1];
+		/*printf("%d a[i]:\n", a[i]);*/
 		a[n - i - 1] = temp;
+		/*printf("%d a[m-i-1]:\n", a[n-i-1]);*/
 	}
 
+	/*
+	*printf("%d", a[i]),//6 where 1 < n/2 = 6, swap 5 times BTW start and end
+	*temp = a[i] means temp is holding value at index of d/ array (0)
+	*a[i] = a[n - i - 1], means a[i] = a[13 - 0 - 1],  a[0] = a[12]
+	*next: a[12] = a[0];
+	*/
 
-	/*printf("\n");*/
 }
