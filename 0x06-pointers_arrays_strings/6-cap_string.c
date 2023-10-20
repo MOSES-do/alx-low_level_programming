d1d07d018f764da0295cccc242a8f98f7c6ebdd0
Esumei Moses <archer.me24@gmail.com>
2023-10-20 06:25:34 -0700

    cap-string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,18 +13,44 @@ int separator(char *sign)
 
 	while (sign[i])
 	{
+
+		if (sign[i -1] == ' ' ||
+			sign[i - 1] == '\t' ||
+			sign[i - 1] == '\n' ||
+			sign[i - 1] == ',' ||
+			sign[i - 1] == ';' ||
+			sign[i - 1] == '!' ||
+			sign[i - 1] == '?' ||
+			sign[i - 1] == '"' ||
+			sign[i - 1] == '(' ||
+			sign[i - 1] == ')' ||
+			sign[i - 1] == '{' ||
+			sign[i - 1] == '}' ||
+			sign[i -1] == '.')
+		{
+			if (sign[i] > 'A' && sign[i] > 'Z')
+				sign[i] = sign[i] - 32;
+		}
+		
+		if(sign[i] == '\t')
+			sign[i] = ' ';
+
+		/*
 		if (sign[i] == ';' || sign[i] == '\t' || sign[i] == '?')
 			sign[i] = ' ';
 
 
 		if (sign[i] == ' ' || sign[i] == '\n')
 		{
-			if (sign[i + 1] > 'A' && sign[i + 1] > 'Z')
+			if (sign[i + 1] >= 'A' && sign[i + 1] <= 'Z')
 				sign[i + 1] = sign[i + 1] - 32;
 		}
 
+		if (sign[i - 1] == '\t' || sign[i - 1] == ','
+
 		if (sign[i] == 'd' && sign[i + 1] == '.')
 			sign[i + 2] = sign[i + 2] - 32;
+		*/
 
 	i++;
 	}
