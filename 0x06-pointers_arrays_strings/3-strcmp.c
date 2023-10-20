fc6446059f2ab03ec30a845e5029c9410ff92c8b
Esumei Moses <archer.me24@gmail.com>
2023-10-19 20:05:58 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,8 +5,8 @@
 * @s1: param 1
 * @s2: param 2
 * Return:	If at any point in the loop the index of  s1 < s2 / differs -
-*		from s2  return negative 15
-*		If the index of s1 > s2 return positive 15
+*		from s2  return the difference
+*		If the index of s1 > s2 return the difference
 *		If s1 == s2 or s1 == s1 return 0
 */
 
@@ -14,9 +14,16 @@
 int _strcmp(char *s1, char *s2)
 {
 
-	while (*s1 != '\0' && *s2 != '\0')
-	{
-		return (*s1 - *s2);
+	while (*s1  && *s2)
+	{	
+		/*
+		putchar(*s1++);
+		putchar(*s2++);
+		
+		printf("character:\n");
+		*/
+	
+		return (*s1++ - *s2++);
 	}
 
 	return (0);
