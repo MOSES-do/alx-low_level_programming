c61751fe283c0e415323e7cf3ca864e8702ae263
Esumei Moses <archer.me24@gmail.com>
2023-10-19 18:26:02 -0700

    strcmps

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,17 +17,9 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[index] != '\0' && s2[index] != '\0')
 	{
-		if (s1[index] < s2[index])
-		{
-			return (-15);
-		}
-		else if (s1[index] > s2[index])
-		{
-			return (s1 - s2);
-		}
-		else
-		{
-			return (0);
-		}
+		if (s1[index] && s2[index])
+			return *s1 - *s2;
 	}
+
+	return (0);
 }
