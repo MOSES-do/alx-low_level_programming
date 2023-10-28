f5299deaba8a5c46ed36b5b0ae9f2715a9123b2a
Esumei Moses <archer.me24@gmail.com>
2023-10-27 19:00:05 -0700

    argc, *argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+* main - function
+* @argc: param 1
+* @argv[]: param 2
+* Return: Always 0 Success
+*/
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", *argv);
+	return (argc);
+}
