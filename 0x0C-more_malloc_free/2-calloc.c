f80075d0650b79bfef9c787cc2cf8216e69c5cee
Esumei Moses <archer.me24@gmail.com>
2023-11-06 11:33:42 -0800

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -26,7 +26,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (memory == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb - 1; i++)
 		memory[i] = '\0';
 
 	return (memory);
