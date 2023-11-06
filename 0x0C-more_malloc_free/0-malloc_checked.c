991fd8723c4d52441de4983d3a34cae71b994849
Esumei Moses <archer.me24@gmail.com>
2023-11-06 06:22:12 -0800

    0-malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
 
+/**
+* malloc_checked - function
+* @b: param
+* Return: Always 0 (Success)
+*/
+
 
 
 void *malloc_checked(unsigned int b)
 {
-	
 	void *add = malloc(b);
 
 	if (add == NULL)
