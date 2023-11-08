bbe5bc6e5d182254f4ac54cfa218d8de246d876c
Esumei Moses <archer.me24@gmail.com>
2023-11-07 20:24:32 -0800

    2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,13 +12,14 @@ void print_dog(struct dog *d)
 {
 
 
-if (d != NULL)
-{
+if (d == NULL)
+	return;
+
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 	{
-		printf("Name: %s\n", d->name);	
+	printf("Name: %s\n", d->name);
 	}
 
 	if (d->age < 0)
@@ -27,8 +28,8 @@ if (d != NULL)
 	{
 		printf("Age: %f\n", d->age);
 	}
-	
+
 	printf("Owner: %s\n", d->owner);
 
-}
+
 }
