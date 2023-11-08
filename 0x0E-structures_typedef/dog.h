391805f050f4351ee7eb460ef146d344ce3abec3
Esumei Moses <archer.me24@gmail.com>
2023-11-07 18:26:16 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+* dog - structue of dog 
+* @name: Dog's name
+* @age: Age of the owner
+* @owner: Dog owner's name
+*/
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
