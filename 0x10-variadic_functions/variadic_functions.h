53edf73749b9f401675cf7dedd1deb8e0d65112a
Esumei Moses <archer.me24@gmail.com>
2023-11-09 16:41:14 -0800

    sum_all-0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,22 +11,22 @@
  *         a data type corresponding to symbol.
  */
 
-/*
-typedef struct printer
-{
-	char *symbol;
-	void (*print)(va_list arg);
-
-} printer_t;
-*/
+/**
+ * typedef struct printer
+ * {
+ *	char *symbol;
+ *	void (*print)(va_list arg);
+ *
+ * } printer_t;
+ */
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
-/*
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+/**
+ * void print_numbers(const char *separator, const unsigned int n, ...);
+ * void print_strings(const char *separator, const unsigned int n, ...);
+ * void print_all(const char * const format, ...);
 */
 
 #endif;
