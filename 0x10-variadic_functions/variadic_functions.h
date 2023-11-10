f34e37a58832cfe94b5e51186ca751a0c2befa0e
Esumei Moses <archer.me24@gmail.com>
2023-11-09 16:34:52 -0800

    sum_all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -20,8 +20,11 @@ typedef struct printer
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+
+/*
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+*/
 
 #endif;
