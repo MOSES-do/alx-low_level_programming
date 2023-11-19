68203baeb5924cd8a8b6f72376a018dc89da4fe7
Esumei Moses <archer.me24@gmail.com>
2023-11-19 05:26:46 -0800

    task 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - calling function
+* @array: elements to be passed to void (*action)(int)
+* @size: length of array
+* @action: function pointer
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return;
+
+	for (; i < size; i++)
+	{
+		action(array[i]);
+	}
+
+
+}
