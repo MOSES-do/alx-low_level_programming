09e0623f26553140c60a7a43add753a188037b16
Esumei Moses <archer.me24@gmail.com>
2023-11-28 14:55:43 -0800

    task 3

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* append_text_to_file - writes text to end of a file
+* @filename: name of source file
+* @text_content: string to be appended 
+* Return: 1 on success, -1 on failure
+*/
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int len = 0, fd, wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		len = strlen(text_content);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	wr = write(fd, text_content, len);
+
+	if (fd == -1 || wr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
