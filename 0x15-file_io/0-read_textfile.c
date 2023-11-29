c316d0432d9dc5bda89cb58d1931d0592b175ea3
Esumei Moses <archer.me24@gmail.com>
2023-11-29 01:43:38 -0800

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,13 +2,28 @@
 #include <stdlib.h>
 
 /**
+* createbuffer - function to create buffer length
+* @length: buffer length
+* Return: buffer
+*/
+char *createbuffer(size_t length)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * length);
+
+	if (buffer == NULL)
+		return (0);
+	
+	return (buffer);
+}
+
+/**
 * read_textfile - function to print a string
 * @filename: par that receives atring from man function
 * @letters: length of string to be printed
 * Return: 0 - on fialure;
 */
-
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
@@ -17,10 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
-		return (0);
+	buffer = createbuffer(letters);
 
 	o = open(filename, O_RDONLY);
 	rd = read(o, buffer, letters);
