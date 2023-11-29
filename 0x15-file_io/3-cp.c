5fde993c787d7708ffd103b2b6e991014933a411
Esumei Moses <archer.me24@gmail.com>
2023-11-29 00:18:47 -0800

    TAsk 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,6 +20,11 @@ void close_file(int fd)
 	}
 }
 
+/**
+* createbuf - create buffer to hold text
+* @newfile: file to receive buffer
+* Return: buffer
+*/
 char *createbuf(char *newfile)
 {
 	char *buffer;
@@ -35,50 +40,6 @@ char *createbuf(char *newfile)
 	return (buffer);
 }
 
-/**
-* cp_text_to_file - copies content from one file to another
-* @file_from: file to copy
-* @file_to: destination file from src
-* Return: 0 on success
-*/
-
-int cp_text_to_file(const char *file_from, char *file_to)
-{
-	char *buffer;
-	int FD_VALUE, FROM, rd, wr;
-
-	buffer = createbuf(file_to);
-	FROM = open(file_from, O_RDONLY);
-	rd = read(FROM, buffer, BUFFER_SIZE);
-	FD_VALUE = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	wr = write(FD_VALUE, buffer, rd);
-	do {
-		if (FROM == -1 || rd == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			free(buffer);
-			exit(98);
-		}
-
-		if (FD_VALUE == -1 || wr == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			free(buffer);
-			exit(99);
-		}
-
-		rd = read(FROM, buffer, 1024);
-		FD_VALUE = open(file_to, O_WRONLY | O_APPEND);
-
-	} while (rd > 0);
-
-	free(buffer);
-	close_file(FROM);
-	close_file(FD_VALUE);
-
-	return (0);
-}
-
 
 /**
 * main - copies contnent of one file to another
@@ -89,8 +50,8 @@ int cp_text_to_file(const char *file_from, char *file_to)
 
 int main(int ac, char **argv)
 {
-
-	
+	char *buffer;
+	int FD_VALUE, FROM, rd, wr;
 
 	if (ac != 3)
 	{
@@ -98,5 +59,36 @@ int main(int ac, char **argv)
 		exit(97);
 	}
 
-	return cp_text_to_file(argv[1], argv[2]);
+	buffer = createbuf(argv[2]);
+	FROM = open(argv[1], O_RDONLY);
+	rd = read(FROM, buffer, BUFFER_SIZE);
+	FD_VALUE = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	wr = write(FD_VALUE, buffer, rd);
+
+	do {
+		if (FROM == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		if (FD_VALUE == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		rd = read(FROM, buffer, 1024);
+		FD_VALUE = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (rd > 0);
+
+	free(buffer);
+	close_file(FROM);
+	close_file(FD_VALUE);
+
+	return (0);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,13 +5,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int cp_text_to_file(const char *file_from, char *file_to);
 #endif
