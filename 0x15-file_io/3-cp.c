069a500bdc43afc9520bd2cc834887b6f1ed6267
Esumei Moses <archer.me24@gmail.com>
2023-11-28 20:53:00 -0800

    task 4

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include "main.h"
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+/**
+* append_text_to_file - copies content from one file to another
+* @file_from: file to copy
+* @file_to: destination file from src
+* 
+*/
+
+int cp_text_to_file(const char *file_from, char *file_to)
+{
+	char *buffer;
+	int FD_VALUE, FROM, rd, wr;
+
+	buffer = malloc(sizeof(char) * BUFFER_SIZE);
+
+	if (buffer == NULL)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+        	exit(99);
+	}
+
+	FROM = open(file_from,O_RDONLY);
+	rd = read(FROM, buffer, BUFFER_SIZE);
+	FD_VALUE = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	wr = write(FD_VALUE, buffer, rd);
+
+	if (FROM == -1 || rd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		free(buffer);
+        	exit(98);
+	}
+
+	if (FD_VALUE == -1 || wr == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+		free(buffer);
+        	exit(99);
+	}
+
+	free(buffer);
+	close(FROM);
+	close(FD_VALUE);
+
+	if (FD_VALUE == -1)
+	{
+		dprintf(2, "Can't close fd %d\n", FD_VALUE);
+        	exit(100);	
+	}
+	return (0);
+}
+
+
+/**
+*
+* main - copies contnent of one file to another
+* @ac: number of args
+* @argv: array of pointers to args
+* Return: 0 on success
+*/
+
+int main(int ac, char **argv)
+{
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	cp_text_to_file(argv[1], argv[2]);
+
+	return (0);
+}
