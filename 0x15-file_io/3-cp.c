64369a8f4f38027954e268647726489400a60f9a
Esumei Moses <archer.me24@gmail.com>
2023-11-28 23:18:45 -0800

    task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,11 +44,11 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
+ * @ac: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  * Return: 0 on success.
  */
-int main(int ac, char *argv[])
+int main(int ac, char **argv)
 {
 	int FROM, FD_VALUE, rd, w;
 	char *buffe;
