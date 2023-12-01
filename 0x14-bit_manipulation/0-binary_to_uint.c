6dc178f585184cae8cdb98299e1da444bd7480eb
Esumei Moses <archer.me24@gmail.com>
2023-11-30 16:16:00 -0800

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,11 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+* swapbuffer - swaps array's 1st and last elements using a loop
+* @buf: array of numbers
+* @len: length of array
+* @b: string of binary numbers
+* Return:sum of array;
+*/
 unsigned int swapbuffer(int *buf, int len, const char *b)
 {
 	 int tmp, i, halves;
 	 unsigned int sum = 0;
-	
+
 	if (len % 2)
 	{
 		halves = (len / 2) + 1;
@@ -32,26 +40,32 @@ unsigned int swapbuffer(int *buf, int len, const char *b)
 	free(buf);
 }
 
+/**
+* binary_to_uint - function to add binary numbers
+*	in  string that aren't zero to the power of 2
+* @b: binary string to be converted to numbers
+* Return: function that returns sum
+*/
 unsigned int binary_to_uint(const char *b)
-{	
+{
 	int *buffer, len, n = 0, i, base = 2;
+
 	len = strlen(b);
 
 	buffer = malloc(sizeof(int) * len);
 
 	if (buffer == NULL)
 		return (1);
-	
 
 	for (i = 0; i < len; i++)
 	{
 		if (b[i] >= 'a' && b[i] <= 'z')
 			return (0);
 
-		n += 1;	
+		n += 1;
 		buffer[i] = n;
 		if (n > 1)
-		{	
+		{
 			buffer[i] = buffer[i - 1] * base;
 		}
 	}
