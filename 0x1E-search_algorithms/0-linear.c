32970024ef8f49751570afdc27a313b2bd4d22f7
Esumei Moses <archer.me24@gmail.com>
2024-05-09 21:44:08 -0700

    linear & biinary searches

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
 
 /**
- * linear_search - functiom to search an array linearly 0(n) complexity 
+ * linear_search - functiom to search an array linearly 0(n) complexity
+ * @array: data structure to be traversed
  * @size: size of the array/no. of element sin the array
  * @value: element to be founs within the array
  * Return: -1 /if element is not found/
@@ -9,12 +10,13 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
+
 	while (i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
+			return (i);
 	i++;
 	}
-	return -1;
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,8 @@
 #include "search_algos.h"
 /**
- * binary_search - function to search an array logarithmacally 0(log n) complexity
+ * binary_search - function to search an array
+ *	logarithmacally 0(log n) complexity
+ * @array: d.s to be traversed
  * @size: size of the array/no. of element sin the array
  * @value: element to be founs within the array
  * Return: -1 /if element is not found/
@@ -14,7 +16,6 @@ int binary_search(int *array, size_t size, int value)
 
 	while (left <= right)
 	{
-		
 		mid = left + (right - left) / 2;
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
@@ -22,11 +23,11 @@ int binary_search(int *array, size_t size, int value)
 			printf("%d", array[i]);
 			if (i < right)
 				printf(", ");
-		}	
+		}
 		printf("\n");
 
 		if (array[mid] == value)
-			return mid;
+			return (mid);
 		if (array[mid] < value)
 		{
 			left = mid + 1;
@@ -36,5 +37,5 @@ int binary_search(int *array, size_t size, int value)
 			right = mid - 1;
 		}
 	}
-	return -1;
+	return (-1);
 }
