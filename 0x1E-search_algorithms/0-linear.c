40a3ab0e5bfdce6de9a7364a5109b88a57fa504a
Esumei Moses <archer.me24@gmail.com>
2024-05-09 21:35:38 -0700

    linear and binary search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,12 +1,11 @@
 #include "search_algos.h"
 
 /**
- * 
- *
- *
+ * linear_search - functiom to search an array linearly 0(n) complexity 
+ * @size: size of the array/no. of element sin the array
+ * @value: element to be founs within the array
+ * Return: -1 /if element is not found/
  */
-
-
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+/**
+ * binary_search - function to search an array logarithmacally 0(log n) complexity
+ * @size: size of the array/no. of element sin the array
+ * @value: element to be founs within the array
+ * Return: -1 /if element is not found/
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid = 0;
+	size_t i = 0;
+	size_t left = 0;
+	size_t right = size - 1;
+
+	while (left <= right)
+	{
+		
+		mid = left + (right - left) / 2;
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+		}	
+		printf("\n");
+
+		if (array[mid] == value)
+			return mid;
+		if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+	return -1;
+}
