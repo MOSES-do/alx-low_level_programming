30cb99c36ca0a82b264f185bd076f2bab28e1152
Esumei Moses <archer.me24@gmail.com>
2024-05-11 02:41:44 -0700

    100-jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,58 +1,85 @@
 #include "search_algos.h"
+
 /**
- * jump_search - jump algorithmn
- * @array: array parameter
- * @size: size parameter
- * @value: value parameter
- * Return: -1
+ * *asc_order_array - sorts array in asc order
+ * @array: Array to be sorted
+ * @size: length of array
+ * Return: returns sorted array
  */
-
-int jump_search(int *array, size_t size, int value)
+int *asc_order_array(int *array, size_t size)
 {
-	int jump = sqrt(size);
-	int low = 0;
-	int j = size - 1;
+	int i = 0;
+	int j = 0;
+	int n = size - 1;
+	int temp = 0;
 
-	if (array == NULL)
-		return (-1);
-	while (array[low] <= value && low < jump)
+	while (i < n)
 	{
-		if (array[low] == value)
-			return (low);
-		if (low <= j)
+		for (j = i; j <= n; j++)
 		{
-			printf("Value checked array[%d] = [%d]\n", low, array[low]);
-			 if (((array[low] <= value) && (array[jump] >= value)) || (((low == j) && (array[jump] <= value || array[jump] >= value))))
+			if (array[i] > array[j])
 			{
-				int i = low;
-
-				printf("Value found between indexes [%d] and [%d]\n", low, jump);
-				while (i <= jump)
-				{
-					if (i <= j)
-						printf("Value checked array[%d] = [%d]\n", i, array[i]);
-					if (array[i] == value)
-						return (i);
-					i++;
-				}
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
 			}
 		}
-		if (low > j)
-		{
-			if (array[low] <= value && array[jump] >= value)
-			{
-				int i = low;
+	i++;
+	}
+	return (array);
+}
 
-				while (i <= jump)
-				{
-					if (array[i] == value)
-						return (-1);
-					i++;
-				}
-			}
+
+/**
+ * jump_search - function to search an array
+ *	using a movement based on sqrt of
+ *	size of array - 1, i.e. n = size-1.
+ *	Using sqrt(n) as the basis for movement
+ *	within the array
+ * @array: d.s to be traversed
+ * @size: size of the array/no. of element sin the array
+ * @value: element to be founs within the array
+ * Return: -1 /if element is not found/
+ */
+int jump_search(int *array, size_t size, int value)
+{/**
+* Control out of bound only when step is
+* a sqrt higher than and not when step == n
+* if size = 10-1 = 9, control out of bound when step = 12
+* NOT NEEDED -	controls out of bound at exactly n
+* define min(a, b) ((a) < (b) ? (a) : (b))
+*/
+	int step = 0;
+	int prev = 0;
+	int *arr;
+	int n = size - 1;
+
+	arr = asc_order_array(array, size);
+
+	if (arr == NULL)
+		return (-1);
+	while (arr[step] < value && prev != n)
+	{
+		if (prev == n)
+			return (-1);
+		printf("Value checked array[%d] = [%d]\n", step, arr[step]);
+		prev = step;
+		step += sqrt(n);
+	}
+	printf("Value found between indexes [%d] and [%d]\n", prev, step);
+	while (prev <= step)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, arr[prev]);
+		prev++;
+
+		if (arr[prev] == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", prev, arr[prev]);
+			return (prev);
 		}
-		low = jump;
-		jump += sqrt(size);
+
+		if (step > n)
+			return (-1);
 	}
 	return (-1);
 }
